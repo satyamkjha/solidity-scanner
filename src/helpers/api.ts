b0ce473b95eaaa0654fed036a71f2488de76e2ff
Shashank <Shashank-In@users.noreply.github.com>
2021-07-05 15:38:03 +0530

    Update api.ts

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import Auth from './auth';
 
-export const API_URL_PROD = `http://solidityscan.com:8000`;
+export const API_URL_PROD = `https://solidityscan.com:8000`;
 export const API_URL_DEV = `http://127.0.0.1:8000/`;
 
 const API = axios.create({
