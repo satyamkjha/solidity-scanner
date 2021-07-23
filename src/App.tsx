391aa4c639ddbd044b9a145435990c374307304e
Soumik Sur <soumiksur.me@gmail.com>
2021-07-23 13:07:26 +0530

    added hubspot code

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 
 import React, { Suspense } from "react";
+import { Helmet } from "react-helmet";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -16,6 +17,15 @@ const queryClient = new QueryClient();
 
 export const App = () => (
   <Suspense fallback="">
+    <Helmet>
+      <script
+        type="text/javascript"
+        id="hs-script-loader"
+        async
+        defer
+        src="//js-eu1.hs-scripts.com/24889894.js"
+      ></script>
+    </Helmet>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <Routes />
