78340208ad3f220e04c8954a9aa44e5eca484223
Soumik Sur <soumiksur.me@gmail.com>
2021-10-15 12:14:07 +0530

    fixed type error

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -22,5 +22,5 @@ export function timeSince(date: Date): string {
 export function getCookie(name: string) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(";").shift();
+  if (parts.length === 2) return (parts.pop() || "").split(";").shift();
 }
