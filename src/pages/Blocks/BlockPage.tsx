1e97eeee26b706faefb1999eb61f90bab996b067
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2022-03-15 18:24:21 +0530

    FIX: stopping get reports api call if report not generated

--- src/pages/Blocks/BlockPage.tsx
+++ src/pages/Blocks/BlockPage.tsx
@@ -474,6 +474,20 @@ const BlockPage: React.FC = () => {
                               {scanData.scan_report.currency}
                             </Text>
                           </VStack>
+                          <VStack textAlign={"left"} width={"33.33%"}>
+                            <Text
+                              width={"100%"}
+                              as="p"
+                              fontSize="14px"
+                              color="gray.500"
+                              mt={10}
+                            >
+                              Contract Chain
+                            </Text>
+                            <Text width={"100%"} as="p" fontSize="14px">
+                              {scanData?.scan_report.contract_chain}{" "}
+                            </Text>
+                          </VStack>
                         </Flex>
                       </AccordionPanel>
                     </>
@@ -517,7 +531,9 @@ const BlockPage: React.FC = () => {
                     }
                   </TabPanel>
                   <TabPanel>
-                    <PublishedReports scan_report={scanData.scan_report} />
+                      {scanData.scan_report.project_id && (
+                        <PublishedReports scan_report={scanData.scan_report} />
+                    )}
                   </TabPanel>
                 </TabPanels>
               </Tabs>
