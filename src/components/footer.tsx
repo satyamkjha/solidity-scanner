5553094c688bcce3e35c7de548603e0c466faea1
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2022-04-13 11:10:50 +0530

    footer and no bugs screen

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,24 +1,27 @@
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
-import { Flex, Container, Link } from "@chakra-ui/react";
+import { Link as RouterLink, useHistory } from "react-router-dom";
+import { Flex, Container, Link, Image } from "@chakra-ui/react";
 
 import { Logo } from "components/icons";
 export const Footer: React.FC = () => {
+
+  const history = useHistory()
+
   return (
     <Container maxW="80vw" my={20}>
       <Flex
         flexDirection={["column", "column", "row"]}
         justifyContent="space-between"
       >
-        <Flex flexDirection="column" width={["100%", "100%", "33%"]}>
+        <Flex flexDirection="row" alignItems={'flex-start'} width={["100%", "100%", "100%"]}>
           <Logo />
-        </Flex>
-        <Flex
+          <Flex
           width={["100%", "100%", "33%"]}
           flexWrap="wrap"
           my={[4, 0]}
+          mx={[0, 20]}
           pt={[10, 10, 0]}
-          textAlign="center"
+          textAlign="left"
         >
           <Link
             as={RouterLink}
@@ -60,6 +63,31 @@ export const Footer: React.FC = () => {
           >
             Privacy Policy
           </Link>
+          </Flex>
+        </Flex>
+        <Flex
+          width={["100%", "100%", "40%"]}
+          justifyContent='flex-end'
+          alignItems={'center'}
+        >
+          {/* <Image
+              src="/facebook-social.svg"
+              alt="Facebook"
+              mx={3}
+              onClick={()=> window.open('https://www.instagram.com/credshields/', '_blank')}
+          /> */}
+          <Image
+              src="/instagram-social.svg"
+              alt="Instagram"
+              onClick={()=> window.open('https://www.instagram.com/credshields/', '_blank')}
+              mx={3}
+          />
+          <Image
+              src="/twitter-social.svg"
+              alt="Twitter"
+              mx={3}
+              onClick={()=> window.open('https://twitter.com/CredShields', '_blank')}
+          />
         </Flex>
       </Flex>
     </Container>
