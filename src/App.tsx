614cf8f6321c643ca2caf9a91acd77170b8ec407
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2022-12-15 10:25:38 +0530

    react helmet to habdle hotjar code for prod/dev

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,8 @@ const GlobalStyles = css`
   }
 `;
 
+
+
 export const App = () => (
   <Suspense fallback="">
     <Helmet>
@@ -41,6 +43,21 @@ export const App = () => (
         defer
         src="//js-eu1.hs-scripts.com/24889894.js"
       ></script>
+      {process.env.NODE_ENV === "development" ? <></>: <script>
+      {`(function (h, o, t, j, a, r) {
+        h.hj =
+          h.hj ||
+          function () {
+            (h.hj.q = h.hj.q || []).push(arguments);
+          };
+        h._hjSettings = { hjid: 3239945, hjsv: 6 };
+        a = o.getElementsByTagName("head")[0];
+        r = o.createElement("script");
+        r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");`}
+    </script>}
     </Helmet>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
