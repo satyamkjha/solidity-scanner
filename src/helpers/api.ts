3c07dd04450e575d6f997366b437fedd738a66ea
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2023-01-06 11:02:44 +0530

    Billing page UI Optimisation: payment modal && transaction hsitory

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -4,6 +4,8 @@ import axios from "axios";
 export const API_URL_PROD = process.env.REACT_APP_API_URL_PROD;
 export const API_URL_DEV = process.env.REACT_APP_API_URL_DEV;
 
+console.log(API_URL_DEV);
+
 const API = axios.create({
   baseURL: process.env.NODE_ENV === "production" ? API_URL_PROD : API_URL_DEV,
   headers: {
