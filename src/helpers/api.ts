9bb3c5dda5ae552cbfd1fdf21e85358d87dfea6a
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2023-01-23 15:59:41 +0530

    removed api file cal

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -6,7 +6,7 @@ export const API_URL_PROD = process.env.REACT_APP_API_URL_PROD;
 export const API_URL_DEV = process.env.REACT_APP_API_URL_DEV;
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8010",
+  baseURL: process.env.NODE_ENV === "production" ? API_URL_PROD : API_URL_DEV,
   headers: {
     "Content-Type": "application/json",
   },
