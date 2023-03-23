6dbb72b198f19754b7faef733b6a8bf937c30049
LALIT MOHAN <lalit@credshields.com>
2023-03-23 13:20:39 +0530

    add + in regex

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -64,7 +64,7 @@ const Profile: React.FC = () => {
   }, [data]);
 
   const validateEmail = (value: string) => {
-    const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+    const emailRegex = /^([a-zA-Z0-9._%-+]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
     if (!emailRegex.test(value)) {
       setError("Please enter a valid email address.");
       return false;
