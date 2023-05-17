cd33517c74a769a26ab2aceca31126b436874139
LALIT MOHAN <lalit@credshields.com>
2023-05-17 12:12:47 +0530

    fix check icon css

--- src/components/styled-components/inputCheckbox.tsx
+++ src/components/styled-components/inputCheckbox.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 import { CheckIcon } from "@chakra-ui/icons";
 
 interface InputCheckboxProps {
-  width?: string;
-  height?: string;
+  width?: number;
+  height?: number;
   checkedColor?: string;
   unCheckedColor?: string;
   checked: boolean;
@@ -12,23 +12,23 @@ interface InputCheckboxProps {
 }
 
 const InputCheckbox: React.FC<InputCheckboxProps> = ({
-  width = "16px",
-  height = "16px",
+  width = 16,
+  height = 16,
   checkedColor = "#805AD5",
   unCheckedColor = "gray.200",
   checked,
   onChange,
 }) => {
   return (
-    <Flex alignItems="center" mr={checked ? "2px" : 0}>
+    <Flex alignItems="center" justifyContent={"center"}>
       <input
         id={"sol-checkbox"}
         type={"checkbox"}
         checked={checked}
         onChange={onChange}
         style={{
-          width: `${width}`,
-          height: `${height}`,
+          width: `${width}px`,
+          height: `${height}px`,
           WebkitAppearance: "none",
           appearance: "none",
           backgroundColor: `${unCheckedColor}`,
@@ -37,13 +37,26 @@ const InputCheckbox: React.FC<InputCheckboxProps> = ({
         }}
       />
       {checked && (
-        <CheckIcon
-          color="white"
-          ml={"-14px"}
-          w={3}
-          h={3}
-          pointerEvents="none"
-        />
+        <Box
+          style={{
+            position: "relative",
+            display: "inline-block",
+            width: `${width}px`,
+            height: `${height}px`,
+            marginLeft: `${-width}px`,
+            pointerEvents: "none",
+          }}
+        >
+          <CheckIcon
+            position="absolute"
+            top={`${height / 2}px`}
+            left={`${width / 2}px`}
+            transform="translate(-50%, -50%)"
+            color="white"
+            w={3}
+            h={3}
+          />
+        </Box>
       )}
       <style>
         {`#sol-checkbox:checked {
