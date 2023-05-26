75ef46a1ea8c92ab26919d0e6509dba279a49376
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2023-05-26 10:34:30 +0530

    upload file progress revamp wip

--- src/pages/Home/UploadForm.tsx
+++ src/pages/Home/UploadForm.tsx
@@ -27,6 +27,28 @@ import { useProfile } from "hooks/useProfile";
 import { useDropzone } from "react-dropzone";
 import { API_PATH } from "helpers/routeManager";
 
+type UrlItemProps = { url: string; name: string; file: File };
+
+const UrlItem: React.FC<{ item: UrlItemProps }> = ({ item }) => {
+  return (
+    <VStack width="100%" my={4}>
+      <HStack justify={"space-between"} width="100%">
+        <HStack align={"flex-end"}>
+          <SolidityFileIcon size={25} />
+          <Text fontSize={"14px"}>{item.name}</Text>
+        </HStack>
+        <HStack align={"flex-end"}>
+          <Text color={"gray.500"}>Uploading...</Text>
+          <Spinner color={"gray.500"} />
+          <Text fontSize={"15px"}>|</Text>
+          <CloseButton onClick={() => {}} />
+        </HStack>
+      </HStack>
+      <Progress width="100%" variant={"blue"} size="xs" isIndeterminate />
+    </VStack>
+  );
+};
+
 const UploadForm: React.FC = () => {
   const history = useHistory();
   const { data: profileData } = useProfile();
@@ -37,9 +59,7 @@ const UploadForm: React.FC = () => {
   const [error, setError] = useState(false);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [name, setName] = useState("");
-  const [urlList, setUrlList] = useState<
-    { url: string; name: string; file: File }[]
-  >([]);
+  const [urlList, setUrlList] = useState<UrlItemProps[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -331,7 +351,7 @@ const UploadForm: React.FC = () => {
                 {/* </>
                   )} */}
               </div>
-            ) : step === 1 ? (
+            ) : (
               <Box
                 sx={{ w: "100%", borderRadius: "20px", p: 10, my: 2 }}
                 justifyContent="flex-start"
@@ -339,80 +359,10 @@ const UploadForm: React.FC = () => {
                 background={"#FFFFFF"}
                 border={"1.5px dashed #D6D6D6"}
               >
-                <HStack justify={"space-between"}>
-                  <HStack align={"flex-end"} my={4}>
-                    <SolidityFileIcon size={25} />
-                    <Text fontSize={"14px"}>{acceptedFiles[0].name}</Text>
-                    <Text fontSize={"15px"}>|</Text>
-                    <Text fontSize={"10px"} color={"gray.500"}>
-                      0{acceptedFiles.length} files
-                    </Text>
-                  </HStack>
-                  <CloseButton
-                    onClick={() => {
-                      setStep(0);
-                      setUrlList([]);
-                    }}
-                  />
-                </HStack>
-                <Progress variant={"blue"} size="xs" isIndeterminate />
-                <HStack mt={4} justify={"space-between"}>
-                  <Text color={"gray.500"}>Uploading...</Text>
-                  <Spinner color={"gray.500"} />
-                </HStack>
+                {urlList.map((item) => (
+                  <UrlItem item={item} />
+                ))}
               </Box>
-            ) : (
-              <>
-                <Box
-                  sx={{
-                    w: "100%",
-                    borderRadius: "20px",
-                    px: 20,
-                    pt: 2,
-                    pb: 10,
-                    my: 2,
-                  }}
-                  justifyContent="flex-start"
-                  alignItems={"flex-start"}
-                  background={"#FFFFFF"}
-                  border={"1.5px dashed #D6D6D6"}
-                  maxH="300px"
-                  overflowY={"scroll"}
-                >
-                  <VStack h="fit-content" spacing={2} width="100%">
-                    <HStack width="100%" justify={"flex-end"}>
-                      <CloseButton
-                        onClick={() => {
-                          setStep(0);
-                          setUrlList([]);
-                        }}
-                      />
-                    </HStack>
-                    <HStack>
-                      <ProjectIcon size={30} />
-                      <Text>{name}</Text>
-                    </HStack>
-                    <Text fontSize={"10px"} color={"gray.500"}>
-                      0{acceptedFiles.length} files
-                    </Text>
-                    {acceptedFiles.map((file) => (
-                      <Box
-                        width={"100%"}
-                        justifyContent={"center"}
-                        alignItems="center"
-                        textAlign={"center"}
-                        fontSize="13px"
-                        borderRadius={4}
-                        color="gray.500"
-                        backgroundColor="#F8FAFC"
-                        py={3}
-                      >
-                        {file.name}
-                      </Box>
-                    ))}
-                  </VStack>
-                </Box>
-              </>
             )}
 
             <Button
