af661391b7e88945531b7fcd29c9437f5954391a
litz044 <lalit@credshields.com>
2023-08-07 11:03:06 +0530

    use fetch config api

--- src/helpers/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG || "");
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getDatabase(app);
