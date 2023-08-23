0760fc3f798f9b81bb12712d3d2bdcdbd08da6bc
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2023-08-23 09:53:26 +0530

    fixed pro account current plan description issue

--- src/pages/Billing/components/CurrentPlan.tsx
+++ src/pages/Billing/components/CurrentPlan.tsx
@@ -166,105 +166,107 @@ const CurrentPlan: React.FC<{
           justifyContent={"flex-start"}
           alignItems="flex-start"
         >
-          <Flex
-            w="100%"
-            maxW={
-              packageName === "trial" || packageName === "ondemand"
-                ? "375px"
-                : "400px"
-            }
-            maxH="185px"
-            px={8}
-            py={6}
-            background={
-              packageName === "trial" || packageName === "ondemand"
-                ? "linear-gradient(101.8deg, #000000 4.3%, #3E1EA8 108.23%)"
-                : packageName
-            }
-            backgroundImage={
-              packageName === "trial" || packageName === "ondemand"
-                ? `url('${assetsURL}pricing/pro_upgrade.svg')`
-                : "none"
-            }
-            backgroundSize="contain"
-            backgroundRepeat="no-repeat"
-            borderRadius="15px"
-            flexDir="column"
-          >
-            {packageName === "trial" || packageName === "ondemand" ? (
-              <>
-                <Text fontSize="xl" color="white" mt={4}>
-                  Upgrade to <strong>Pro</strong>
-                </Text>
-                <Text
-                  color="white"
-                  fontWeight="400"
-                  fontSize="sm"
-                  w="75%"
-                  mt="2"
-                >
-                  You've subscribed to a free trial version, Upgrade to unlock
-                  features and starts scanning your contracts.
-                </Text>
-              </>
-            ) : (
-              <>
-                <Flex>
-                  {subscription && (
-                    <VStack alignItems="flex-start" spacing={1}>
-                      <Text fontSize="xs" fontWeight="400">
-                        Next Billed on
+          {packageName !== "custom" && (
+            <Flex
+              w="100%"
+              maxW={
+                packageName === "trial" || packageName === "ondemand"
+                  ? "375px"
+                  : "400px"
+              }
+              maxH="185px"
+              px={8}
+              py={6}
+              background={
+                packageName === "trial" || packageName === "ondemand"
+                  ? "linear-gradient(101.8deg, #000000 4.3%, #3E1EA8 108.23%)"
+                  : packageName
+              }
+              backgroundImage={
+                packageName === "trial" || packageName === "ondemand"
+                  ? `url('${assetsURL}pricing/pro_upgrade.svg')`
+                  : "none"
+              }
+              backgroundSize="contain"
+              backgroundRepeat="no-repeat"
+              borderRadius="15px"
+              flexDir="column"
+            >
+              {packageName === "trial" || packageName === "ondemand" ? (
+                <>
+                  <Text fontSize="xl" color="white" mt={4}>
+                    Upgrade to <strong>Pro</strong>
+                  </Text>
+                  <Text
+                    color="white"
+                    fontWeight="400"
+                    fontSize="sm"
+                    w="75%"
+                    mt="2"
+                  >
+                    You've subscribed to a free trial version, Upgrade to unlock
+                    features and starts scanning your contracts.
+                  </Text>
+                </>
+              ) : (
+                <>
+                  <Flex>
+                    {subscription && (
+                      <VStack alignItems="flex-start" spacing={1}>
+                        <Text fontSize="xs" fontWeight="400">
+                          Next Billed on
+                        </Text>
+                        <Text fontSize="sm" fontWeight="600">
+                          {formattedDate(
+                            new Date(subscription.renewal_date),
+                            "long"
+                          )}
+                        </Text>
+                      </VStack>
+                    )}
+                    {packageName === "pro" && (
+                      <Image
+                        src={`${assetsURL}pricing/pro_badge.svg`}
+                        ml="auto"
+                        mt={-8}
+                        h={"80px"}
+                      />
+                    )}
+                  </Flex>
+                  <Flex mt={isCancellable ? 6 : 2} align="center">
+                    <CircularProgress
+                      value={
+                        subscription
+                          ? getNextPaymentValue(
+                              new Date(packageRechargeDate),
+                              new Date(subscription.renewal_date)
+                            )
+                          : getNextPaymentValue(new Date(packageRechargeDate))
+                      }
+                      color={packageName + "-dark"}
+                      trackColor="white"
+                      thickness="8px"
+                      size="60px"
+                      capIsRound
+                    ></CircularProgress>
+                    <VStack alignItems="flex-start" spacing={0} ml={3}>
+                      <Text fontSize="lg" fontWeight="700">
+                        {subscription
+                          ? getPaymentDaysLeft(
+                              new Date(subscription.renewal_date)
+                            )
+                          : getPaymentDaysLeft()}
+                        &nbsp; days
                       </Text>
-                      <Text fontSize="sm" fontWeight="600">
-                        {formattedDate(
-                          new Date(subscription.renewal_date),
-                          "long"
-                        )}
+                      <Text fontSize="sm" fontWeight="400">
+                        remaining for the {sentenceCapitalize(plan.name)} Plan
                       </Text>
                     </VStack>
-                  )}
-                  {packageName === "pro" && (
-                    <Image
-                      src={`${assetsURL}pricing/pro_badge.svg`}
-                      ml="auto"
-                      mt={-8}
-                      h={"80px"}
-                    />
-                  )}
-                </Flex>
-                <Flex mt={isCancellable ? 6 : 2} align="center">
-                  <CircularProgress
-                    value={
-                      subscription
-                        ? getNextPaymentValue(
-                            new Date(packageRechargeDate),
-                            new Date(subscription.renewal_date)
-                          )
-                        : getNextPaymentValue(new Date(packageRechargeDate))
-                    }
-                    color={packageName + "-dark"}
-                    trackColor="white"
-                    thickness="8px"
-                    size="60px"
-                    capIsRound
-                  ></CircularProgress>
-                  <VStack alignItems="flex-start" spacing={0} ml={3}>
-                    <Text fontSize="lg" fontWeight="700">
-                      {subscription
-                        ? getPaymentDaysLeft(
-                            new Date(subscription.renewal_date)
-                          )
-                        : getPaymentDaysLeft()}
-                      &nbsp; days
-                    </Text>
-                    <Text fontSize="sm" fontWeight="400">
-                      remaining for the {sentenceCapitalize(plan.name)} Plan
-                    </Text>
-                  </VStack>
-                </Flex>
-              </>
-            )}
-          </Flex>
+                  </Flex>
+                </>
+              )}
+            </Flex>
+          )}
           <Flex mt={[6, 6, 6, "auto"]} ml={[0, 0, 0, 4]}>
             <Button
               variant="accent-outline"
