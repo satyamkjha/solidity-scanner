a20e7e42268b596cce9b5a79bfe802f6079f33a8
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2024-01-18 11:27:39 +0530

    changed links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -97,7 +97,7 @@ export const Header: React.FC<{ theme: "light" | "dark" }> = ({ theme }) => {
               variant={linkVariant}
               fontWeight="600"
             >
-              HackBoard
+              Web3 HackHub
             </Link>
             <Menu matchWidth>
               <MenuButton
@@ -260,7 +260,7 @@ export const Header: React.FC<{ theme: "light" | "dark" }> = ({ theme }) => {
                       p={1}
                       ml={3}
                     >
-                      HackBoard
+                      Web3 HackHub
                     </Link>
                   </MenuItem>
                   <MenuItem>
