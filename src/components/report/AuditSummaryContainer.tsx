00806202615b87a55457452507bf86c13be2afc4
litz044 <lalit@credshields.com>
2024-01-22 17:41:11 +0530

    add description change

--- src/components/report/AuditSummaryContainer.tsx
+++ src/components/report/AuditSummaryContainer.tsx
@@ -278,34 +278,43 @@ const AuditSummaryContainer: React.FC<{
           </Text>
         </VStack>
       </Flex>
-      <Text fontSize="xs" fontWeight={400}>
-        This report has been prepared for{" "}
-        {summary_report.project_summary_report.project_name} using SolidityScan
-        to scan and discover vulnerabilities and safe coding practices in their
-        smart contract including the libraries used by the contract that are not
-        officially recognized. The SolidityScan tool runs a comprehensive static
-        analysis on the Solidity code and finds vulnerabilities ranging from
-        minor gas optimizations to major vulnerabilities leading to the loss of
-        funds. The coverage scope pays attention to all the informational and
-        critical vulnerabilities with over (100+) modules. The scanning and
-        auditing process covers the following areas:{" "}
-      </Text>
+      {summary_report.project_summary_report.description ? (
+        <Text fontSize="xs" fontWeight={400}>
+          {summary_report.project_summary_report.description}
+        </Text>
+      ) : (
+        <>
+          <Text fontSize="xs" fontWeight={400}>
+            This report has been prepared for{" "}
+            {summary_report.project_summary_report.project_name} using
+            SolidityScan to scan and discover vulnerabilities and safe coding
+            practices in their smart contract including the libraries used by
+            the contract that are not officially recognized. The SolidityScan
+            tool runs a comprehensive static analysis on the Solidity code and
+            finds vulnerabilities ranging from minor gas optimizations to major
+            vulnerabilities leading to the loss of funds. The coverage scope
+            pays attention to all the informational and critical vulnerabilities
+            with over (100+) modules. The scanning and auditing process covers
+            the following areas:{" "}
+          </Text>
 
-      <Text fontSize="xs" fontWeight={400} mt={2}>
-        Various common and uncommon attack vectors will be investigated to
-        ensure that the smart contracts are secure from malicious actors. The
-        scanner modules find and flag issues related to Gas optimizations that
-        help in reducing the overall Gas cost It scans and evaluates the
-        codebase against industry best practices and standards to ensure
-        compliance It makes sure that the officially recognized libraries used
-        in the code are secure and up to date
-      </Text>
-      <Text fontSize="xs" fontWeight={400} mt={2}>
-        The SolidityScan Team recommends running regular audit scans to identify
-        any vulnerabilities that are introduced after{" "}
-        {summary_report.project_summary_report.project_name} introduces new
-        features or refactors the code.
-      </Text>
+          <Text fontSize="xs" fontWeight={400} mt={2}>
+            Various common and uncommon attack vectors will be investigated to
+            ensure that the smart contracts are secure from malicious actors.
+            The scanner modules find and flag issues related to Gas
+            optimizations that help in reducing the overall Gas cost It scans
+            and evaluates the codebase against industry best practices and
+            standards to ensure compliance It makes sure that the officially
+            recognized libraries used in the code are secure and up to date
+          </Text>
+          <Text fontSize="xs" fontWeight={400} mt={2}>
+            The SolidityScan Team recommends running regular audit scans to
+            identify any vulnerabilities that are introduced after{" "}
+            {summary_report.project_summary_report.project_name} introduces new
+            features or refactors the code.
+          </Text>
+        </>
+      )}
     </Flex>
   );
 };
