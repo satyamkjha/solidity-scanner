c886e6d59ed6c0572024ea701f5853e1f71abead
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2024-03-11 11:23:39 +0530

    fixed issue

--- src/components/modals/ScanErrorModal.tsx
+++ src/components/modals/ScanErrorModal.tsx
@@ -17,6 +17,7 @@ import { RescanIcon } from "components/icons";
 import { ScanTitleComponent } from "./InScanModal";
 import { useHistory } from "react-router-dom";
 import ModalBlurOverlay from "components/common/ModalBlurOverlay";
+import { useWebSocket } from "hooks/useWebhookData";
 
 const ScanErrorModal: React.FC<{
   onClose: any;
@@ -25,6 +26,32 @@ const ScanErrorModal: React.FC<{
   insufficientMsg?: any;
 }> = ({ isOpen, onClose, inScanDetails, insufficientMsg }) => {
   const history = useHistory();
+  const { sendMessage } = useWebSocket();
+
+  const rescan = () => {
+    if (inScanDetails.scan_type === "project") {
+      sendMessage({
+        type: "project_scan_initiate",
+        body: {
+          project_id: inScanDetails.project_id,
+          project_type: "existing",
+        },
+      });
+    } else {
+      let req = {};
+          req = {
+            contract_address: inScanDetails.contract_address,
+            contract_platform: inScanDetails.contract_platform,
+            contract_chain: inScanDetails.contract_chain,
+          };
+          sendMessage({
+            type: "block_scan_initiate",
+            body: req,
+          });
+        }
+    }
+  };
+
   return (
     <Modal
       isCentered
@@ -104,40 +131,45 @@ const ScanErrorModal: React.FC<{
                   : inScanDetails.scan_status ||
                     "This scan has failed, lost credits will be reimbursed in a few minutes. Please contact support"}
               </Text>
-              <Flex
-                w="100%"
-                justifyContent="space-between"
-                alignItems="center"
-                mt={5}
-              >
-                <HStack spacing={5}>
-                  <RescanIcon size={80} />
-                  <Text
-                    w="100%"
-                    overflowWrap="break-word"
-                    isTruncated
-                    fontWeight={600}
-                    fontSize="md"
-                  >
-                    Rescan Project
-                  </Text>
-                </HStack>
-                {inScanDetails.scan_type === "project" &&
-                  inScanDetails.project_url !== "File Scan" && (
-                    <Button
-                      onClick={() =>
-                        history.push(
-                          `/${inScanDetails.scan_type}s/${inScanDetails.project_id}/${inScanDetails.scan_id}`
-                        )
-                      }
-                      variant="text"
-                      minW="150px"
-                      color="#3300FF"
+              {!(
+                inScanDetails.scan_type === "project" &&
+                inScanDetails.project_url === "File Scan"
+              ) && (
+                <Flex
+                  w="100%"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  mt={5}
+                >
+                  <HStack onClick={() => {}} spacing={5}>
+                    <RescanIcon size={80} />
+                    <Text
+                      w="100%"
+                      overflowWrap="break-word"
+                      isTruncated
+                      fontWeight={600}
+                      fontSize="md"
                     >
-                      View Scan History
-                    </Button>
-                  )}
-              </Flex>
+                      Rescan Project
+                    </Text>
+                  </HStack>
+                  {inScanDetails.scan_type === "project" &&
+                    inScanDetails.project_url !== "File Scan" && (
+                      <Button
+                        onClick={() =>
+                          history.push(
+                            `/${inScanDetails.scan_type}s/${inScanDetails.project_id}/${inScanDetails.scan_id}`
+                          )
+                        }
+                        variant="text"
+                        minW="150px"
+                        color="#3300FF"
+                      >
+                        View Scan History
+                      </Button>
+                    )}
+                </Flex>
+              )}
             </Flex>
           </Flex>
         </ModalBody>
