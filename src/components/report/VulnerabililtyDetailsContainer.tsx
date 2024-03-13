defb9781a5ce2098fec0dafda024c77939679368
Satyam Jha <satyamjha@Satyams-MacBook-Air.local>
2024-03-13 15:30:06 +0530

    fixed issues

--- src/components/report/VulnerabililtyDetailsContainer.tsx
+++ src/components/report/VulnerabililtyDetailsContainer.tsx
@@ -24,6 +24,9 @@ import React from "react";
 import { ExternalLinkIcon, LockIcon } from "@chakra-ui/icons";
 import { useHistory } from "react-router-dom";
 import { codePlatform } from "common/values";
+import { ReportPayWall } from "./ReportPayWall";
+import { DemoCodeBlock } from "./DemoCodeBlock";
+import { ReportCodeBlock } from "./ReportCodeBlock";
 
 const VulnerabililtyDetailsContainer: React.FC<{
   summary_report: Report;
@@ -56,52 +59,6 @@ const VulnerabililtyDetailsContainer: React.FC<{
 }) => {
   const assetsURL = getAssetsURL();
   const history = useHistory();
-  const getFileContent = (
-    file_path: string,
-    line_start: number,
-    line_end: number
-  ) => {
-    const file = filesContent.filter((item) => item.path === file_path);
-
-    if (file.length === 0) {
-      return [];
-    }
-
-    let dataArray = file[0].content.split("\n");
-
-    line_start = Math.max(0, line_start - 2);
-    line_end = Math.min(dataArray.length - 1, line_end + 1);
-
-    dataArray = [...dataArray];
-
-    return dataArray.slice(line_start, line_end + 1);
-  };
-
-  const getCodeLineNo = (start: number, index: number) => {
-    return start === 0 ? index + 1 : start - 2 + index + 1;
-  };
-
-  const demoCodeArray = [
-    "",
-    "    function callback(uint256[] memory ids, uint256 campaignId)onlyOracle public {",
-    '        require(winningTicketIds[campaignId].length + ids.length <= campaigns[campaignId].rewardCount, "exceeded reward limit");',
-    "",
-    "        for(uint256 i = 0; i<ids.length; i++){",
-    "             winningTicketIds[campaignId].push(ids[i]);",
-    "            winningTixketIdExist[campaignId][ids[i]] = true;",
-    "        }",
-    "        for(uint256 i = 0; i<ids.length; i++){",
-    "             winningTicketIds[campaignId].push(ids[i]);",
-    "            winningTixketIdExist[campaignId][ids[i]] = true;",
-    "        }",
-    "        for(uint256 i = 0; i<ids.length; i++){",
-    "             winningTicketIds[campaignId].push(ids[i]);",
-    "            winningTixketIdExist[campaignId][ids[i]] = true;",
-    "        }",
-    "    }",
-    "",
-    "    function setOracleAddress(address _add) onlyOwner public {",
-  ];
 
   const demoIssueDescription =
     "Access control plays an important role in segregation of privileges in smart contracts and other applications. If this is misconfigured or not properly validated on sensitive functions, it may lead to loss of funds, tokens and in some cases compromise of the smart contract.";
@@ -147,13 +104,6 @@ const VulnerabililtyDetailsContainer: React.FC<{
       sm: 6,
       md: 10,
     }) || 10;
-
-  let codeFontSize =
-    useBreakpointValue({
-      base: "4px",
-      sm: "5px",
-      md: "10px",
-    }) || "10px";
 
   let descFontSize =
     useBreakpointValue({
@@ -484,159 +434,23 @@ const VulnerabililtyDetailsContainer: React.FC<{
           </>
         ) : null}
 
-        {type !== "desc" ? (
-          issue.findings ? (
-            <Flex
-              w={"100%"}
-              h={showDescription ? "auto" : "100%"}
-              px={download ? 6 : [2, 3, 6]}
-              mb={showDescription ? 0 : download ? 4 : [1, 2, 4]}
-              flexDir={"column"}
-              borderLeft={"1px solid #D9D9D9"}
-              borderRight={"1px solid #D9D9D9"}
-            >
-              {issue.findings.map((item, index) => (
-                <Flex
-                  key={index}
-                  w={"100%"}
-                  flexDir={"column"}
-                  bg={"#FBFBFB"}
-                  borderRadius={15}
-                  mb={download ? 4 : [1, 2, 4]}
-                  p={download ? 4 : [1, 2, 4]}
-                >
-                  <Flex w={"100%"} pb={download ? 3 : [1, 2, 3]}>
-                    <Text
-                      fontSize={download ? "10px" : ["6px", "7px", "10px"]}
-                      color={"#323B4B"}
-                    >
-                      {item.file_path}
-                    </Text>
-                    <Text
-                      fontSize={download ? "10px" : ["6px", "7px", "10px"]}
-                      color={"#323B4B"}
-                      ml={"auto"}
-                    >
-                      L{item.line_nos_start} - L{item.line_nos_end}
-                    </Text>
-                  </Flex>
-                  <Divider mb={download ? 3 : [1, 2, 3]} />
-                  <Flex w={"100%"} flexDir={"column"}>
-                    {getFileContent(
-                      item.file_path,
-                      item.file_path === type && codeStartLine
-                        ? codeStartLine
-                        : item.line_nos_start[0],
-                      item.file_path === type && codeEndLine
-                        ? codeEndLine
-                        : item.line_nos_end[0]
-                    ).map((content: any, cIndex: number, array: any[]) => (
-                      <HStack
-                        as={"div"}
-                        key={cIndex}
-                        align={"flex-start"}
-                        spacing={download ? 4 : [1, 2, 4]}
-                        pb={download ? 1 : ["1px", "2px", 1]}
-                      >
-                        <Text
-                          color={"#D8D8D8"}
-                          fontSize={codeFontSize}
-                          fontWeight="normal"
-                        >
-                          {codeStartLine
-                            ? getCodeLineNo(codeStartLine, cIndex)
-                            : getCodeLineNo(item.line_nos_start[0], cIndex)}
-                        </Text>
-                        <pre
-                          style={{
-                            fontSize: download ? "10px" : codeFontSize,
-                            color:
-                              (cIndex > 0 && cIndex < array.length - 2) ||
-                              item.line_nos_start[0] === 0
-                                ? "#000000"
-                                : "#B0B7C3",
-                            whiteSpace: "pre-wrap",
-                          }}
-                          key={cIndex}
-                        >
-                          {content}
-                        </pre>
-                      </HStack>
-                    ))}
-                  </Flex>
-                </Flex>
-              ))}
-            </Flex>
+        {type !== "desc" &&
+          (issue.findings ? (
+            <ReportCodeBlock
+              download={download}
+              findings={issue.findings}
+              type={type}
+              showDescription={showDescription}
+              codeStartLine={codeStartLine}
+              codeEndLine={codeEndLine}
+              filesContent={filesContent}
+            />
           ) : (
-            <Flex
-              w={"100%"}
-              h={showDescription ? "auto" : "100%"}
-              px={download ? 6 : [2, 3, 6]}
-              mb={showDescription ? 0 : download ? 4 : [1, 2, 4]}
-              flexDir={"column"}
-              borderLeft={"1px solid #D9D9D9"}
-              borderRight={"1px solid #D9D9D9"}
-            >
-              <Flex
-                w={"100%"}
-                flexDir={"column"}
-                bg={"#FBFBFB"}
-                borderRadius={15}
-                mb={download ? 4 : [1, 2, 4]}
-                p={download ? 4 : [1, 2, 4]}
-              >
-                <Flex w={"100%"} pb={download ? 3 : [1, 2, 3]}>
-                  <Text
-                    fontSize={download ? "10px" : ["6px", "7px", "10px"]}
-                    color={"#323B4B"}
-                  >
-                    {"item.file_path"}
-                  </Text>
-                  <Text
-                    fontSize={download ? "10px" : ["6px", "7px", "10px"]}
-                    color={"#323B4B"}
-                    ml={"auto"}
-                  >
-                    L{"110"} - L{"120"}
-                  </Text>
-                </Flex>
-                <Divider mb={download ? 3 : [1, 2, 3]} />
-                <Flex w={"100%"} flexDir={"column"}>
-                  {demoCodeArray.map(
-                    (content: any, cIndex: number, array: any[]) => (
-                      <HStack
-                        as={"div"}
-                        key={cIndex}
-                        align={"flex-start"}
-                        spacing={download ? 4 : [1, 2, 4]}
-                        pb={download ? 1 : ["1px", "2px", 1]}
-                      >
-                        <Text
-                          color={"#D8D8D8"}
-                          fontSize={codeFontSize}
-                          fontWeight="normal"
-                        ></Text>
-                        <pre
-                          style={{
-                            fontSize: download ? "10px" : codeFontSize,
-                            color: "#B0B7C3",
-                            whiteSpace: "pre-wrap",
-                          }}
-                          key={cIndex}
-                        >
-                          {content}
-                        </pre>
-                      </HStack>
-                    )
-                  )}
-                </Flex>
-              </Flex>
-            </Flex>
-          )
-        ) : null}
+            <DemoCodeBlock download={download} />
+          ))}
 
-        {showDescription ? (
-          issue.issue_remediation && issue.issue_description ? (
+        {showDescription &&
+          (issue.issue_remediation && issue.issue_description ? (
             <>
               <Flex
                 w={"100%"}
@@ -858,125 +672,22 @@ const VulnerabililtyDetailsContainer: React.FC<{
                 </Flex>
               )}
             </>
-          )
-        ) : null}
+          ))}
 
         {!issue.findings && (
-          <Flex
-            w="96%"
-            left="2px"
-            top={
-              showVulnerabilityTitle
-                ? download
-                  ? "270px"
-                  : ["150px", "190px", "270px"]
-                : download
-                ? "190px"
-                : ["110px", "140px", "190px"]
+          <ReportPayWall
+            issue_count={
+              summary_report.scan_summary[0].issue_severity_distribution[
+                issue.severity
+              ]
             }
-            h={["280px", "400px", "730px"]}
-            position="absolute"
-            sx={{
-              backdropFilter: "blur(6px)",
-            }}
-            bg="rgba(255,255,255,0.3)"
-            alignItems="center"
-            justifyContent="center"
-            flexDir="column"
-          >
-            <Flex
-              w="90%"
-              borderRadius={10}
-              p={[3, 4, 7]}
-              flexDir={"column"}
-              alignItems="center"
-              justifyContent="flex-start"
-              bg="linear-gradient(rgba(238, 235, 255, 1), rgba(229, 246, 255, 1))"
-            >
-              <HStack w="100%">
-                <VStack
-                  alignItems={["center", "center", "flex-start"]}
-                  textAlign="left"
-                  w={["100%", "100%", "calc(100% - 200px)"]}
-                  spacing={[4, 4]}
-                >
-                  <Text color="#000000" fontWeight={600} fontSize="md">
-                    {isQSReport
-                      ? issue.severity === "gas"
-                        ? "Free Unlimited Gas Issues"
-                        : "Reveal Detailed Vulnerabilities"
-                      : "Upgrade your Plan to view the full report"}
-                  </Text>
-                  <Text
-                    color="#000000"
-                    fontWeight={300}
-                    fontSize={["xs", "sm"]}
-                  >
-                    <b>
-                      {
-                        summary_report.scan_summary[0]
-                          .issue_severity_distribution[issue.severity]
-                      }{" "}
-                      {sentenceCapitalize(issue.severity)} Issues Found
-                    </b>
-                  </Text>
-                  <Text
-                    color="#000000"
-                    fontWeight={300}
-                    textAlign={["center", "center", "left"]}
-                    fontSize={["xs", "sm"]}
-                  >
-                    {isQSReport
-                      ? issue.severity === "gas"
-                        ? "Sign up for a free trial and optimize your contracts for gas absolutely free!"
-                        : "Make a one-time payment and get a detailed security report for your smart contract with security scores, bug descriptions & remediations directly in your inbox!"
-                      : "Please upgrade your plan to view all the issues in your report."}
-                  </Text>
-                  <Button
-                    leftIcon={
-                      issue.severity !== "gas" ? <LockIcon /> : undefined
-                    }
-                    variant="brand"
-                    w="90%"
-                    minWidth="200px"
-                    onClick={() => {
-                      if (isQSReport) {
-                        if (issue.severity === "gas") {
-                          history.push("/signup");
-                          onImportScan();
-                        } else {
-                          onOpen();
-                        }
-                      } else {
-                        history.push("/billing");
-                      }
-                    }}
-                  >
-                    {isQSReport
-                      ? issue.severity === "gas"
-                        ? "Signup for Free Trial"
-                        : "Unlock report"
-                      : "Upgrade"}
-                  </Button>
-                </VStack>
-                <Image
-                  display={["none", "none", "block"]}
-                  height="200px"
-                  width="200px"
-                  style={{
-                    mixBlendMode: "hard-light",
-                  }}
-                  src={`${assetsURL}report/paywall.svg`}
-                />
-              </HStack>
-              <Image
-                display={["none", "none", "block"]}
-                height="390px"
-                width="auto"
-                src={`${assetsURL}report/paywall_screenshot.svg`}
-              />
-            </Flex>
-          </Flex>
+            issue_severity={issue.severity}
+            showVulnerabilityTitle={showVulnerabilityTitle}
+            download={download}
+            isQSReport={isQSReport}
+            onImportScan={onImportScan}
+            onOpen={onOpen}
+          />
         )}
       </Flex>
     </Flex>
